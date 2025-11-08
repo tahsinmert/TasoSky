1634fa5c7891dc908a359d5e08eb7d0fa51bde1a
Tahsin Mert MUTLU <tahsinmert.mutlu@std.yeditepe.edu.tr>
2025-11-08 18:32:56 +0300

    Initial Commit

---
+++ TasoSky/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TasoSky
+//
+//  Created by Tahsin Mert Mutlu on 08.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TasoSky/TasoSkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasoSkyApp.swift
+//  TasoSky
+//
+//  Created by Tahsin Mert Mutlu on 08.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct TasoSkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
